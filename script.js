b89070e076c2027ac8a2bbf72a3d23701b8d6d59
abdullah-rasheed-techno-soft <abdullah.rasheed@techno-soft.com>
2022-08-18 18:30:10 +0500

    Updated

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+  const showNavbar = (toggleId, navId, bodyId) => {
     const toggle = document.getElementById(toggleId),
       nav = document.getElementById(navId),
-      bodypd = document.getElementById(bodyId),
-      headerpd = document.getElementById(headerId);
+      bodypd = document.getElementById(bodyId);
+    // headerpd = document.getElementById(headerId);
 
     // Validate that all variables exist
-    if (toggle && nav && bodypd && headerpd) {
+    if (toggle && nav && bodypd) {
       toggle.addEventListener("click", () => {
         // show navbar
         nav.classList.toggle("show");
@@ -15,12 +15,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
         // add padding to body
         bodypd.classList.toggle("body-pd");
         // add padding to header
-        headerpd.classList.toggle("body-pd");
+        // headerpd.classList.toggle("body-pd");
       });
     }
   };
 
-  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+  showNavbar("header-toggle", "nav-bar", "body-pd");
 
   /*===== LINK ACTIVE =====*/
   const linkColor = document.querySelectorAll(".nav_link");
