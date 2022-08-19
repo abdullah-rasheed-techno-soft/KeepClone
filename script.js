5a434facfd86157e90ae07787450ea6ab413935e
abdullah-rasheed-techno-soft <abdullah.rasheed@techno-soft.com>
2022-08-19 18:15:51 +0500

    Recent

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const showNavbar = (toggleId, navId, bodyId) => {
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId),
-      bodypd = document.getElementById(bodyId);
-    // headerpd = document.getElementById(headerId);
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   const showNavbar = (toggleId, navId, bodyId) => {
+//     const toggle = document.getElementById(toggleId),
+//       nav = document.getElementById(navId),
+//       bodypd = document.getElementById(bodyId);
+//     // headerpd = document.getElementById(headerId);
 
-    // Validate that all variables exist
-    if (toggle && nav && bodypd) {
-      toggle.addEventListener("click", () => {
-        // show navbar
-        nav.classList.toggle("show");
-        // change icon
-        toggle.classList.toggle("bx-x");
-        // add padding to body
-        bodypd.classList.toggle("body-pd");
-        // add padding to header
-        // headerpd.classList.toggle("body-pd");
-      });
-    }
-  };
+//     // Validate that all variables exist
+//     if (toggle && nav && bodypd) {
+//       toggle.addEventListener("click", () => {
+//         // show navbar
+//         nav.classList.toggle("show");
+//         // change icon
+//         toggle.classList.toggle("bx-x");
+//         // add padding to body
+//         bodypd.classList.toggle("body-pd");
+//         // add padding to header
+//         // headerpd.classList.toggle("body-pd");
+//       });
+//     }
+//   };
 
-  showNavbar("header-toggle", "nav-bar", "body-pd");
+//   showNavbar("header-toggle", "nav-bar", "body-pd");
 
-  /*===== LINK ACTIVE =====*/
-  const linkColor = document.querySelectorAll(".nav_link");
+//   /*===== LINK ACTIVE =====*/
+//   const linkColor = document.querySelectorAll(".nav_link");
 
-  function colorLink() {
-    if (linkColor) {
-      linkColor.forEach((l) => l.classList.remove("active"));
-      this.classList.add("active");
-    }
-  }
-  linkColor.forEach((l) => l.addEventListener("click", colorLink));
+//   function colorLink() {
+//     if (linkColor) {
+//       linkColor.forEach((l) => l.classList.remove("active"));
+//       this.classList.add("active");
+//     }
+//   }
+//   linkColor.forEach((l) => l.addEventListener("click", colorLink));
 
-  // Your code to run since DOM is loaded and ready
-});
+//   // Your code to run since DOM is loaded and ready
+// });
+var btnContainer = document.getElementById("SidebarUL");
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("Item");
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName(" active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
