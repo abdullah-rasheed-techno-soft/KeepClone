ce9d00e61f68e73f1330f917f371312c1b1e392a
abdullah-rasheed-techno-soft <abdullah.rasheed@techno-soft.com>
2022-08-24 18:19:30 +0500

    delete method completed, search functionality completed

--- script.js
+++ script.js
@@ -4,18 +4,33 @@ textarea.forEach((area) =>
   area.addEventListener(
     "input",
     (e) => {
-      let scHeight = e.target.scrollHeight;
+      const scHeight = e.target.scrollHeight;
       area.style.height = `${scHeight}px`;
     },
     area.addEventListener("keydown", (e) => {
       if (e.key === "Backspace") {
         area.value = area.value.trim();
-        let scHeight = 21;
+        const scHeight = 21;
         area.style.height = `${scHeight}px`;
       }
     })
   )
 );
+function sidebarAdjust() {
+  const sideElements = document.querySelectorAll(".sidebar-text");
+  console.log(sideElements);
+  sideElements.forEach(function (element, index) {
+    console.log(element);
+  });
+
+  // sideElements.forEach((Elements) => {
+  //   if (Elements.style.display == "none") {
+  //     Elements.style.display = "block";
+  //   } else {
+  //     Elements.style.display = "none";
+  //   }
+  // });
+}
 function clearText(id) {
   const searchInput = document.querySelector(id);
   searchInput.value = "";
@@ -33,45 +48,11 @@ searchInput.addEventListener("input", (e) => {
 const box = document.querySelector(".taking-note");
 document.addEventListener("click", function (event) {
   if (event.target.closest(".taking-note")) return;
-  closingNote();
+  else {
+    closingNote();
+  }
 });
-// document.addEventListener("DOMContentLoaded", function (event) {
-//   const showNavbar = (toggleId, navId, bodyId) => {
-//     const toggle = document.getElementById(toggleId),
-//       nav = document.getElementById(navId),
-//       bodypd = document.getElementById(bodyId);
-//     // headerpd = document.getElementById(headerId);
 
-//     // Validate that all variables exist
-//     if (toggle && nav && bodypd) {
-//       toggle.addEventListener("click", () => {
-//         // show navbar
-//         nav.classList.toggle("show");
-//         // change icon
-//         toggle.classList.toggle("bx-x");
-//         // add padding to body
-//         bodypd.classList.toggle("body-pd");
-//         // add padding to header
-//         // headerpd.classList.toggle("body-pd");
-//       });
-//     }
-//   };
-
-//   showNavbar("header-toggle", "nav-bar", "body-pd");
-
-//   /*===== LINK ACTIVE =====*/
-//   const linkColor = document.querySelectorAll(".nav_link");
-
-//   function colorLink() {
-//     if (linkColor) {
-//       linkColor.forEach((l) => l.classList.remove("active"));
-//       this.classList.add("active");
-//     };
-//   }
-//   linkColor.forEach((l) => l.addEventListener("click", colorLink));
-
-//   // Your code to run since DOM is loaded and ready
-// });
 var btnContainer = document.getElementById("SidebarUL");
 // Get all buttons with class="btn" inside the container
 var btns = btnContainer.getElementsByClassName("Item");
@@ -84,29 +65,28 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 function clickingNote() {
-  let note1 = document.querySelector("#take-a-note");
-  let note2 = document.querySelector("#close-a-note");
+  const note1 = document.querySelector("#take-a-note");
+  const note2 = document.querySelector("#close-a-note");
   let textarea = document.querySelector("#title_input");
 
   note1.style.display = "none";
   note2.style.display = "block";
   textarea = textarea.focus();
-  // textarea.setAttribute("style", "background-color:red;");
-
-  // document.getElementById("title_input").autofocus = "true";
 }
 function closingNote() {
-  let title = document.querySelector("#title_input");
-  let desc = document.querySelector("#note_input");
-  let notes = localStorage.getItem("notes");
+  const title = document.querySelector("#title_input");
+  const desc = document.querySelector("#note_input");
+  const notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
   if (title.value !== "" || desc.value !== "") {
-    notesObj.push([title.value, desc.value]);
+    // notesObj.push([title.value, desc.value]);
+    notesObj = [[title.value, desc.value], ...notesObj];
 
+    console.log(notesObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
   }
 
@@ -117,34 +97,45 @@ function closingNote() {
     displayNotes();
   });
 
-  let note1 = document.querySelector("#take-a-note");
-  let note2 = document.querySelector("#close-a-note");
+  const note1 = document.querySelector("#take-a-note");
+  const note2 = document.querySelector("#close-a-note");
 
   note1.style.display = "block";
   note2.style.display = "none";
 }
 function displayNotes() {
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
+  const notes = localStorage.getItem("notes");
+  let notesObj = [];
+  if (notes !== null) {
     notesObj = JSON.parse(notes);
   }
   let html = "";
   notesObj.forEach(function (element, index) {
     html += `
     <div
-    class="card mb-2 me-2 pb-4"
+    class="card noteCard mb-2 me-2 pb-0"
     style="width: 15.2rem; border: 1px solid #e0e0e0"
   >
     <div class="card-body">
       <h6 class="card-title">${element[0]}</h6>
-      <p class="card-text">${element[1]}</p>
+      <p class="card-text mb-1">${element[1]}</p>
+      <img
+      src="/img/trash.png"
+      width="20"
+      height="20"
+      alt=""
+      srcset=""
+      class="delete-option opacity-75"
+      id="${index}"
+      onclick="deleteNote(this.id)"
+
+    />
     </div>
   </div>`;
   });
-  let notesElm = document.getElementById("notes-area");
+  const notesElm = document.getElementById("notes-area");
   if (notesObj.length != 0) {
+    // location.reload();
     notesElm.innerHTML = html;
   } else {
     notesElm.innerHTML = `<div class="col d-flex flex-column align-items-center mt-5 pt-5 pe-5">
@@ -159,6 +150,46 @@ function displayNotes() {
           <h4 class="mt-3 me-5" style="color: #808688">
             Notes you add appear here
           </h4>
+          
         </div>`;
   }
 }
+//delete
+function deleteNote(index) {
+  const notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  displayNotes();
+}
+// search functionality
+const search = document.getElementById("search_input");
+search.addEventListener("input", function () {
+  const inputVal = search.value.toLowerCase();
+  const noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    const title = element.getElementsByTagName("h6")[0].innerText;
+    const desc = element.getElementsByTagName("p")[0].innerText;
+    const note1 = document.querySelector("#take-a-note");
+    const notesarea = document.querySelector("#notes-area");
+
+    if (search.value !== "") {
+      note1.style.display = "none";
+      notesarea.classList.add("mt-5");
+    } else {
+      note1.style.display = "block";
+      notesarea.classList.remove("mt-5");
+    }
+    if (title.includes(inputVal) | desc.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+    // console.log(cardTxt);
+  });
+});
